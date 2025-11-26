784505ccae5adc793a2b659e95598f6212d0c818
Ardabilly1 <billya749@gmail.com>
2025-11-26 10:10:43 +0700

    feat: add redis as caching storage

--- idempotency/storage.py
+++ idempotency/storage.py
@@ -1,4 +1,6 @@
 import asyncio
+import redis.asyncio as redis
+import json
 from typing import Dict
 from .models import CachedResponse
 
@@ -14,3 +16,33 @@ class MemoryStorage:
     async def set(self, key:str, response: CachedResponse):
         async with self.lock:
             self.store[key] = response
+
+class RedisStorage:
+    def __init__(self, url:str, prefix="idem:"):
+        self.redis = redis.from_url(url, decode_responses=True)
+        self.prefix = prefix
+
+    def _key(self, key: str):
+        return f"{self.prefix}{key}"
+    
+    async def get(self, key:str):
+        raw = await self.redis.get(self._key(key))
+        if not raw:
+            return None
+        
+        data = json.loads(raw)
+        return CachedResponse(
+            status_code=data["status_code"],
+            headers=data["headers"],
+            body=data["body"]
+        )
+    
+    async def set(self, key:str, response: CachedResponse):
+        payload = json.dumps({
+            "status_code":response.status_code,
+            "headers":response.headers,
+            "body":response.body
+        })
+
+        await self.redis.set(self._key(key), payload, ex=60*5)
+
